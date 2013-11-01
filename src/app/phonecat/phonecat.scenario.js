463dc9b372fe8e9e93046031b896c7f2cd3f6572
dude <dude@Cookie.(none)>
2013-11-01 18:14:07 +0200

    fix

--- src/app/phonecat/phonecat.scenario.js
+++ src/app/phonecat/phonecat.scenario.js
@@ -7,6 +7,12 @@ describe('Phone Catalog Module', function() {
 
     it('should filter the phone list as user types into the search box', function() {
       expect(repeater('.phones li').count()).toBe(3);
+
+      input('query').enter('nexus');
+      expect(repeater('.phones li').count()).toBe(1);
+
+      input('query').enter('motorola');
+      expect(repeater('.phones li').count()).toBe(2);
     });
   });
 });
